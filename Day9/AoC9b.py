c907a467a9a74e3a6030d56284a26b2c23d2ec77
Nurek54 <g_sienkiewicz@o2.pl>
2023-12-09 06:44:02 +0100

    Adding day9

---
+++ Day9/AoC9b.py
@@ -0,0 +1,11 @@
+def find_next(row):
+    return 0 if all(num == 0 for num in row) else row[0] - find_next([b - a for a, b in zip(row, row[1:])])
+
+with open("AoC_Day9_Input.txt") as file:
+    rows = [list(map(int, row.split())) for row in file.read().split("\n")]
+
+total = 0
+for row in rows:
+    total += find_next(row)
+
+print(total)
