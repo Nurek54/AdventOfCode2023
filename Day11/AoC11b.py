5e1f75541c0966f09abfae964f974396f1539751
Nurek54 <g_sienkiewicz@o2.pl>
2023-12-11 06:58:40 +0100

    Adding day11

---
+++ Day11/AoC11b.py
@@ -0,0 +1,40 @@
+def read_data(file_path):
+    with open(file_path) as file:
+        return [list(line) for line in file.read().split("\n")]
+
+def calculate_distance(stars, expand, expand_col):
+    distance = 0
+    distance2 = 0
+
+    for i in range(len(stars) - 1):
+        for j in range(i + 1, len(stars)):
+            (x1, y1), (x2, y2) = stars[i], stars[j]
+
+            for num in expand:
+                if x1 < num < x2 or x2 < num < x1:
+                    distance += 1
+                    distance2 += 999999
+
+            for num in expand_col:
+                if y1 < num < y2 or y2 < num < y1:
+                    distance += 1
+                    distance2 += 999999
+
+            distance += abs(x1 - x2) + abs(y2 - y1)
+
+    return distance, distance2
+
+def main():
+    file_path = "AoC_Day11_Input.txt"
+    data = read_data(file_path)
+
+    expand_rows = [x for x, line in enumerate(data) if all(char == "." for char in line)]
+    expand_cols = [col for col in range(len(data[0])) if all(data[row][col] == "." for row in range(len(data)))]
+    stars = [(x, y) for x, line in enumerate(data) for y, col in enumerate(line) if col == "#"]
+
+    distance, distance2 = calculate_distance(stars, expand_rows, expand_cols)
+
+    print("Part 2:", distance2)
+
+if __name__ == "__main__":
+    main()
